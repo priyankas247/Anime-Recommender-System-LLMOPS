5ac328a67a153291117829a57e604d87cf19aaff
priyankas247 <priyanka.s247@gmail.com>
2025-08-25 17:23:28 +0530

    commit

--- src/recommender.py
+++ src/recommender.py
@@ -4,7 +4,7 @@ from src.prompt_template import get_anime_prompt
 
 class AnimeRecommender:
     def __init__(self,retriever,api_key:str,model_name:str):
-        self.llm = ChatGroq(api_key=api_key,model=model_name,temperature=0)
+        self.llm = ChatGroq(groq_api_key=api_key,model=model_name,temperature=0)
         self.prompt = get_anime_prompt()
 
         self.qa_chain = RetrievalQA.from_chain_type(
